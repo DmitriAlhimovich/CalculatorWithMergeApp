8f7f4d73d290eeeee52826b456d2f2a1dbdd91e5
Dmitriy Alhimovich <dmitriy.alhimovich@arcadia.spb.ru>
2021-07-09 13:25:25 +0300

    Add project files.

---
+++ CalculatorWithMerge/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CalculatorWithMerge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
